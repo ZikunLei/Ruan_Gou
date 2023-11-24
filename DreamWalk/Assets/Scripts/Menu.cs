fa42d99b57678cf6fb017e81631fbb6cb6ea031e
ZikunLei <126877246+ZikunLei@users.noreply.github.com>
2023-11-24 22:46:47 +0800

    第一次提交
    菜单、主场景、角色交互和场景切换功能（素材待修改）

---
+++ DreamWalk/Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public AudioMixer audioMixer;
+
+    //开始游戏
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    //退出游戏
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ DreamWalk/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D rb;
+
+    [Header("移动参数")]
+    public float speed = 110f;
+
+    float xVelocity;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        GroundMovement();
+    }
+
+    void GroundMovement()
+    {
+        xVelocity = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(speed*xVelocity, rb.velocity.y);
+
+        FlipDirection();
+    }
+
+    void FlipDirection()
+    {
+        if (xVelocity < 0)
+            transform.localScale = new Vector2(-1, 1);
+        if (xVelocity > 0)
+            transform.localScale = new Vector2(1, 1);
+    }
+
+    void SwitchScene()
+    {
+        SceneManager.LoadScene("Drink");
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "npc" && Input.GetKey(KeyCode.W))
+        {
+            SwitchScene();
+        } 
+    }
+}
